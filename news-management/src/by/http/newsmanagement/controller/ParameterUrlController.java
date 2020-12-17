bd65445c5d049618d712c49bb564e29fefac90bd
IvanLapa <61242303+IvanLapa@users.noreply.github.com>
2020-12-17 18:53:09 +0300

    Add files via upload

--- news-management/src/by/http/newsmanagement/controller/ParameterUrlController.java
+++ news-management/src/by/http/newsmanagement/controller/ParameterUrlController.java
@@ -8,4 +8,5 @@ public class ParameterUrlController {
 	public static final String MAIN_PAGE = "/WEB-INF/jsp/main.jsp";
 	public static final String CREATE_NEWS = "/WEB-INF/jsp/create_news.jsp";
 	public static final String VIEW_ALL_NEWS = "/WEB-INF/jsp/view_all_news.jsp";
+	public static final String ERROR = "/WEB-INF/jsp/error.jsp";
 }
